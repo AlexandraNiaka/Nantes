e3120089e878597d21e943009f02d8ac377ce8c5
alexandra <niaka.alexandra@gmail.com>
2024-02-16 17:23:28 +0200

    script

--- Assets/Scripts/OSC/setValuesVfxOSC.cs
+++ Assets/Scripts/OSC/setValuesVfxOSC.cs
@@ -12,13 +12,46 @@ public class setValuesVfxOSC : MonoBehaviour
     [SerializeField]
     private   List<VisualEffect> vfx;
 
-    // Start is called before the first frame update
+    [Header("VFX Parameters")]
+    public float minScale;
+    public float maxScale;
+    [SerializeField]
+    [ReadOnly] private float fieldScale;
+    public float minIntensity;
+    public float maxIntensity;
+    [SerializeField]
+    [ReadOnly] private float noiseIntensity;
+    public float minSpeed;
+    public float maxSpeed;
+    [SerializeField]
+    [ReadOnly] private float noiseSpeed;
+    public float minFrequency;
+    public float maxFrequency;
+    [SerializeField]
+    [ReadOnly] private float noiseFrequency;
+
+
+    public float minSize;
+    public float maxSize;
+    [SerializeField]
+    [ReadOnly] private float size;
+
+    [SerializeField]
+    [ReadOnly] private bool useAlpha;
+
+    #region Private Fields
+    [SerializeField]
+    [ReadOnly] private float midiMin=0.0f;
+    [SerializeField]
+    [ReadOnly] private float midiMax=1.0f;
+
+    #endregion
     void Start()
     {
         
     }
 
-    // Update is called once per frame
+
     void Update()
     {
         
@@ -38,5 +71,40 @@ public class setValuesVfxOSC : MonoBehaviour
     void SetValuesOSC(float f)
     {
 
+    }
+
+    public void SetNoiseIntensity(float i, VisualEffect vfx)
+    {
+        noiseIntensity = math.remap(midiMin, midiMax, 0.001f, maxIntensity, i);
+        vfx.SetFloat("CellularNoiseIntensity", noiseIntensity);
+    }
+
+    public void SetFieldScale(float i, VisualEffect vfx)
+    {
+        fieldScale = math.remap(midiMin, midiMax, 0.001f, maxScale, i);
+        vfx.SetFloat("FieldScale", fieldScale);
+    }
+
+    public void SetNoiseSpeed(float i, VisualEffect vfx)
+    {
+        noiseSpeed = math.remap(midiMin, midiMax, 0.001f, maxSpeed, i);
+        vfx.SetFloat("NoiseSpeed", noiseSpeed);
+    }
+
+    public void SetNoiseFrequency(float i, VisualEffect vfx)
+    {
+        noiseFrequency = math.remap(midiMin, midiMax, 0.001f, maxFrequency, i);
+        vfx.SetFloat("NoiseFrequency", noiseFrequency);
+    }
+    public void SetSize(float i, VisualEffect vfx)
+    {
+        size = math.remap(midiMin, midiMax, minSize, maxSize, i);
+        vfx.SetFloat("Scale", size);
+    }
+
+    public void HandleAlpha(float i, VisualEffect vfx)
+    {
+        useAlpha = !useAlpha;
+        vfx.SetBool("UseAlphaNoise", useAlpha);
     }
 }
