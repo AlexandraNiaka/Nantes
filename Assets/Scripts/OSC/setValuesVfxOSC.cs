67b5c36a5188421dd3134bd18810bcec087b8846
alexandra <niaka.alexandra@gmail.com>
2024-02-16 16:20:23 +0200

    Update setValuesVfxOSC.cs

--- Assets/Scripts/OSC/setValuesVfxOSC.cs
+++ Assets/Scripts/OSC/setValuesVfxOSC.cs
@@ -1,9 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.VFX;
+using Unity.Mathematics;
+using NaughtyAttributes;
 
 public class setValuesVfxOSC : MonoBehaviour
 {
+
+    [Header("VFX")]
+    [SerializeField]
+    private   List<VisualEffect> vfx;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +22,21 @@ public class setValuesVfxOSC : MonoBehaviour
     void Update()
     {
         
+    }
+
+    void UpdateValues()
+    {
+        for(int i = 0; i < vfx.Count; i++)
+        {
+            if (vfx[i] != null)
+            {
+
+            }
+        }
+    }
+
+    void SetValuesOSC(float f)
+    {
+
     }
 }
