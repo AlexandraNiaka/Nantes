e0305e69ea22c36d8af1b6f42529924e9ffa9e6e
alexandra <niaka.alexandra@gmail.com>
2024-02-22 17:39:25 +0200

    Update RotateAroundSelf.cs

--- Assets/Scripts/RotateAroundSelf.cs
+++ Assets/Scripts/RotateAroundSelf.cs
@@ -10,15 +10,35 @@ public class RotateAroundSelf : MonoBehaviour
     public float y;
     [SerializeField]
     public float z;
+
+    private float xX;
+    private float yY;
+    private float zZ;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(x, y, z);
+        transform.Rotate(xX, yY, zZ);
     }
+
+    public void SetX(float f)
+    {
+        xX = x * f;
+    }
+
+    public void SetY(float f)
+    {
+        yY = y * f;
+    }
+
+    public void SetZ(float f)
+    {
+        zZ = z * f;
+    }
+
 }
